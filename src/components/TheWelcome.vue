0ab2f695f8f69d997469f927c8ffa4523b219834
Aditiya <aditiyaart23@gmail.com>
2023-07-07 14:13:01 +1200

    hello :)

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,21 +1,6 @@
-<script setup>
-import WelcomeItem from './WelcomeItem.vue'
-import DocumentationIcon from './icons/IconDocumentation.vue'
-import ToolingIcon from './icons/IconTooling.vue'
-import EcosystemIcon from './icons/IconEcosystem.vue'
-import CommunityIcon from './icons/IconCommunity.vue'
-import SupportIcon from './icons/IconSupport.vue'
-</script>
-
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>Documentation</template>
-
-    Vue’s
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
-    provides you with all information you need to get started.
-  </WelcomeItem>
+  <div class="container text-white">
+    <h1>Hello</h1>
+    <h2 class="text-center">:)</h2>
+  </div>
 </template>
