cee35ff509f5dac93e30bb807e8534b6bbaed937
Aditiya <aditiyaart23@gmail.com>
2023-07-09 20:24:25 +1200

    center items

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="position-absolute top-50 start-50 translate-middle text-center text-white">
+    <h1>Hello</h1>
+    <h2>:)</h2>
+  </div>
 </template>
