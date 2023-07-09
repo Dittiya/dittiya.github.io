983ac3ee8fe3fa8ce406cb9e04257cbdf19a2429
Aditiya <aditiyaart23@gmail.com>
2023-07-09 19:57:38 +1200

    added footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,7 +3,9 @@
 </script>
 
 <template>
-  <div class="container-flex">
-    <h6 class="text-dark-emphasis">v0.1.0</h6>
-  </div>
+  <footer class="position-absolute bottom-0 start-0 end-0">
+    <div class="container-flex">
+      <h6 class="text-dark-emphasis">v0.1.0</h6>
+    </div>
+  </footer>
 </template>
