1d582fcec8a7fdde86830decc9fe84cb8827829e
Aditiya <aditiyaart23@gmail.com>
2023-07-10 12:08:35 +1200

    cleanup about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,5 @@
 <template>
-  <div class="about">
+  <div>
     <h1>This is an about page</h1>
   </div>
 </template>
-
-<style>
-  .about {
-    background-image: url('https://webusstatic.yo-star.com/ark_us_web/assets/167748390017011083/c59979ff9f8014cd1c82329f156161f1.jpg');
-  }
-</style>
