f321d2f564eaf4afb39701dc0867e606788ea8ed
Aditiya <aditiyaart23@gmail.com>
2023-07-10 16:28:39 +1200

    carousel list rendering

--- src/components/ProjectsItem.vue
+++ src/components/ProjectsItem.vue
@@ -1,3 +1,15 @@
+<script setup>
+import { reactive, ref } from 'vue';
+
+const items = ref([{ name: "test1" }, { name: "test2" }])
+</script>
+
 <template>
-  
+  <div class="carousel-item bg-body text-center" v-for="(item, index) in items" :class="{ 'active': index === 0 }">
+    <h1>{{ item.name }}</h1>
+  </div>
 </template>
+
+<style>
+
+</style>
