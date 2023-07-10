4e0a838d248f0f6c53d46d85504d24070f4642d2
Aditiya <aditiyaart23@gmail.com>
2023-07-11 10:13:51 +1200

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterView } from 'vue-router'
-import FooterComponent from './components/FooterComponent.vue';
+import FooterComponent from './components/FooterComponent.vue'
 import NavigationBar from './components/NavigationBar.vue'
 </script>
 
