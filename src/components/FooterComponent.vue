5c8c20e8b5b7e3ac5ae8e3fd35e1d92857463b57
Aditiya <aditiyaart23@gmail.com>
2023-07-11 09:54:51 +1200

    fetching version

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,11 +1,28 @@
-<script setup>
+<script>
+async function getConfig() {
+  const response = await fetch("https://raw.githubusercontent.com/Dittiya/dittiya.github.io/master/public/config.json")
+  const json = response.json()
+  return json
+}
 
+export default {
+  data() { return { version: null } },
+  created() {
+    this.fetchData()
+  },
+  methods: {
+    async fetchData() {
+      const config = await getConfig()
+      this.version = config.version
+    }
+  }
+}
 </script>
 
 <template>
   <footer class="position-absolute bottom-0 start-0 end-0">
     <div class="container-flex">
-      <h6 class="text-dark-emphasis">v0.1.0</h6>
+      <h6 class="text-dark-emphasis">v{{ version }}</h6>
     </div>
   </footer>
 </template>
