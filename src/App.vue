f7ef4527a408f76826d3dd2bfafd18662fb7a4c3
Aditiya <aditiyaart23@gmail.com>
2023-07-11 10:47:40 +1200

    add conditionals for loading

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,22 @@
 import { RouterView } from 'vue-router'
 import FooterComponent from './components/FooterComponent.vue'
 import NavigationBar from './components/NavigationBar.vue'
+import LoadingState from './components/LoadingState.vue'
+import { ref } from 'vue';
+
+/* TODO
+ * Add loading while fetching config
+ */
+const loading = ref(true)
 </script>
 
 <template>
-	<NavigationBar />
-	<RouterView />
-	<FooterComponent />
+	<template v-if="loading">
+		<LoadingState />
+	</template>
+	<template v-else>
+		<NavigationBar />
+		<RouterView />
+		<FooterComponent />
+	</template>
 </template>
